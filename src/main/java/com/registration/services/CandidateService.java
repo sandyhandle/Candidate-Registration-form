b49affb075a152a06a254e8bd48b52176a8d4f7a
sandyhandle <64911148+sandyhandle@users.noreply.github.com>
2023-06-03 06:30:33 +0530

    Task 2 business logic (#2)
    * Lombok and Logging were added, Now workign on new tasks
    
    * the controllers were combined and deleted unwanted requests

--- src/main/java/com/registration/services/CandidateService.java
+++ src/main/java/com/registration/services/CandidateService.java
@@ -2,26 +2,24 @@ package com.registration.services;
 
 import com.registration.model.Candidate;
 import com.registration.repositories.CandidateRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class CandidateService {
 
     @Autowired
     private CandidateRepository repository;
 
-    public CandidateService(CandidateRepository repository) {
-        this.repository = repository;
-    }
-
     public void save(Candidate candidate){
         repository.save(candidate);
     }
 
-    public Candidate getCandidate(String candidateName, String email){
+    public Candidate getCandidateByNameAndEmail(String candidateName, String email){
         for (Candidate c: repository.findAll()){
             if (c.getCandidateName().equals(candidateName) && c.getEmail().equals(email)){
                 return c;
@@ -30,7 +28,7 @@ public class CandidateService {
         return null;
     }
 
-    public List<Candidate> getAllCandidate(){
+    public List<Candidate> getAllCandidates(){
         return repository.findAll();
     }
 
