83baccdc671bbca7fb0b1eae363560ddad2907bf
sandyhandle <64911148+sandyhandle@users.noreply.github.com>
2023-06-03 06:10:32 +0530

    Lombok and Logging were added, Now workign on new tasks (#1)

--- src/main/java/com/registration/model/Candidate.java
+++ src/main/java/com/registration/model/Candidate.java
@@ -1,27 +1,22 @@
 package com.registration.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "candidate")
+@ToString
+@NoArgsConstructor
+@Getter
+@Setter
 public class Candidate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    public long getId() {
-        return id;
-    }
-
     private int paidAmount = 0;
-
-    public int getPaidAmount() {
-        return paidAmount;
-    }
-
-    public void setPaidAmount(int paidAmount) {
-        this.paidAmount += paidAmount;
-    }
 
     @Column(name = "candidate_name")
     private String candidateName;
@@ -41,35 +36,13 @@ public class Candidate {
     private String gender;
     @Column(name = "skills")
     private String skills;
+
+    public String getCity() {
+        return city;
+    }
+
     @Column(name = "approved")
     private Boolean approved = false;
-
-    public Boolean getApproved() {
-        return approved;
-    }
-
-    public void setApproved(Boolean approved) {
-        this.approved = approved;
-    }
-//    private resume
-
-    @Override
-    public String toString() {
-        return "Candidate{" +
-                "candidateName='" + candidateName + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", qualification='" + qualification + '\'' +
-                ", gender='" + gender + '\'' +
-                ", skills='" + skills + '\'' +
-                '}';
-    }
-
-    public Candidate() {
-    }
 
     public Candidate(String candidateName, String email, String address, String city, String state, String phoneNumber, String qualification, String gender, String skills) {
         this.candidateName = candidateName;
@@ -82,76 +55,7 @@ public class Candidate {
         this.gender = gender;
         this.skills = skills;
     }
-
-    public String getCandidateName() {
-        return candidateName;
-    }
-
-    public void setCandidateName(String candidateName) {
-        this.candidateName = candidateName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getQualification() {
-        return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getSkills() {
-        return skills;
-    }
-
-    public void setSkills(String skills) {
-        this.skills = skills;
+    public void setPaidAmount(int paidAmount) {
+        this.paidAmount += paidAmount;
     }
 }
